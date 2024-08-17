e470f057f091f7ddc0bf1581cebd9fb5b0ec7e40
Syed Tasdeeque Ruhani <130081734+TasRuhani@users.noreply.github.com>
2024-08-18 04:25:07 +0530

    mailGeneration Done

--- server/router/route.js
+++ server/router/route.js
@@ -4,11 +4,12 @@ const router = Router();
 
 /** import all controllers */
 import * as controller from '../controllers/appController.js'
+import { registerMail } from '../controllers/mailer.js'
 import Auth, { localVariables } from '../middleware/auth.js'
 
 /** POST Methods */
 router.route('/register').post(controller.register);//register user
-router.route('/registerMail').post((req, res) => res.end()); //send the email
+router.route('/registerMail').post(registerMail); //send the email
 router.route('/authenticat').post(); //authenticate user
 router.route('/login').post(controller.verifyUser, controller.login); // login in app
 
